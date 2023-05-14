492c5d565209f71873341d4b2578c0ed5bfa8969
Takashi2377 <aka05068@gmail.com>
2023-05-14 15:24:09 +0800

    finish

--- 02-vue-basics/10-computed-vs-methods-vs-watchers/01-computed-vs-methods/before/index.js
+++ 02-vue-basics/10-computed-vs-methods-vs-watchers/01-computed-vs-methods/before/index.js
@@ -8,15 +8,26 @@ const app = Vue.createApp({
         "React 18 新特性介绍",
         "JavaScript 基础语法概览",
       ],
+      counter: 0,
     };
   },
   computed: {
     vueBlogs() {
+      console.log("調用計算屬性函數");
       return this.blogPosts.filter((blog) => blog.includes("Vue"));
+    },
+  },
+  methods: {
+    getVueBlogs(count) {
+      console.log("方法");
+      return this.blogPosts
+        .filter((blog) => blog.includes("Vue"))
+        .slice(0, count);
     },
   },
 });
 app.mount("#app");
 
 //computed 會緩存結果
-//methods 不會緩存結果，每次使用都會重新調用，較耗費效能
+/*methods 不會緩存結果，html中的其他屬性發生變化時，
+methods都會重新執行，較耗費效能*/
