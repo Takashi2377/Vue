66dc3010c3f1d4ed9c7d3461d4259b32d68ca6bf
Takashi2377 <aka05068@gmail.com>
2023-05-15 22:40:04 +0800

    finish

--- 02-vue-basics/13-other-form-controls-event-handling/before/index.js
+++ 02-vue-basics/13-other-form-controls-event-handling/before/index.js
@@ -1,5 +1,13 @@
 // 示例：其它表单控件处理
 const app = Vue.createApp({
-  data() {},
+  data() {
+    return {
+      username: "",
+      gender: "",
+      interest: [],
+      occupation: "",
+      intro: "",
+    };
+  },
 });
 app.mount("#app");
