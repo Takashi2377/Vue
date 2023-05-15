5d2215766583514f5fdc204a75c7401471c2864d
Takashi2377 <aka05068@gmail.com>
2023-05-15 22:14:18 +0800

    finish

--- 02-vue-basics/11-form-input-event-handling/before/index.js
+++ 02-vue-basics/11-form-input-event-handling/before/index.js
@@ -1,3 +1,12 @@
 // 示例：表单输入控件事件处理
-const app = Vue.createApp({});
+const app = Vue.createApp({
+  data() {
+    return { userName: "" };
+  },
+  methods: {
+    handleInput(e) {
+      this.userName = e.target.value;
+    },
+  },
+});
 app.mount("#app");
