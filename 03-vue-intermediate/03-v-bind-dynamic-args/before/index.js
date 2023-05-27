9fe4801192cddc0b5cd19f713e4ed38251aacb43
Takashi2377 <aka05068@gmail.com>
2023-05-27 20:13:56 +0800

    finish

--- 03-vue-intermediate/03-v-bind-dynamic-args/before/index.js
+++ 03-vue-intermediate/03-v-bind-dynamic-args/before/index.js
@@ -1,8 +1,16 @@
 // 示例：给 v-bind 和 v-on 设置动态参数
 const app = Vue.createApp({
   data() {
-    return {};
+    return {
+      attr: "placeholder",
+      value: "請輸入一些字符",
+      event: "change",
+    };
   },
-  methods: {},
+  methods: {
+    handleChange() {
+      console.log("input 變化");
+    },
+  },
 });
 app.mount("#app");
--- 03-vue-intermediate/04-more-about-v-for/before/index.js
+++ 03-vue-intermediate/04-more-about-v-for/before/index.js
@@ -7,6 +7,11 @@ const app = Vue.createApp({
         author: "张旭乾",
         pubDate: "2021-12-12",
       },
+      todos: [
+        { id: 1, content: "待辦1" },
+        { id: 2, content: "待辦2" },
+        { id: 3, content: "待辦3" },
+      ],
     };
   },
 });
