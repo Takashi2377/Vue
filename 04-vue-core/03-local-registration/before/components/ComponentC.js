2cb02c92bae627a446827e95988e7fcefacc010d
Takashi2377 <aka05068@gmail.com>
2023-05-29 20:59:20 +0800

    finish

--- 04-vue-core/03-local-registration/before/components/ComponentC.js
+++ 04-vue-core/03-local-registration/before/components/ComponentC.js
@@ -1,3 +1,8 @@
+import ComponentB from "./ComponentB.js";
+
 export default {
   template: `<p>我是组件 C</p><ComponentB />`,
+  components: {
+    ComponentB,
+  },
 };
--- 04-vue-core/03-local-registration/before/index.js
+++ 04-vue-core/03-local-registration/before/index.js
@@ -3,6 +3,9 @@ import ComponentA from "./components/ComponentA.js";
 import ComponentC from "./components/ComponentC.js";
 
 const app = Vue.createApp({
-  components: {},
+  components: {
+    ComponentA,
+    ComponentC,
+  },
 });
 const vm = app.mount("#app");
