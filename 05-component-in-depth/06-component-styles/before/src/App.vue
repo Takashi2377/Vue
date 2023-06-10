03b3eb85cc3f44c0ca319146feefa6aa19c976dc
Takashi2377 <aka05068@gmail.com>
2023-06-10 12:04:17 +0800

    finish

--- 05-component-in-depth/06-component-styles/before/src/App.vue
+++ 05-component-in-depth/06-component-styles/before/src/App.vue
@@ -1,36 +1,24 @@
 <template>
   <main>
     <div>
-      <p>
-        这是 App.vue 中的样式，它的 style 不带 scoped，所以会影响全局
-      </p>
+      <p>这是 App.vue 中的样式，它的 style 不带 scoped，所以会影响全局</p>
+      <ScopedStyle />
+      <CssModules />
+      <SassStyle />
     </div>
   </main>
 </template>
 
 <script>
-export default {};
+import "./style.css";
+import ScopedStyle from "./components/ScopedStyle.vue";
+import CssModules from "./components/CssModules.vue";
+import SassStyle from "./components/SassStyle.vue";
+
+export default { components: { ScopedStyle, CssModules, SassStyle } };
 </script>
 
 <style>
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-  font-family: Arial, "PingFang SC", "Microsoft Yahei", sans-serif;
-}
-
-body {
-  background-color: #0f141c;
-  opacity: 1;
-  background-image: radial-gradient(
-    #212943 0.6000000000000001px,
-    #0f141c 0.6000000000000001px
-  );
-  background-size: 12px 12px;
-  color: white;
-}
-
 #app {
   width: 100vw;
   height: 100vh;
