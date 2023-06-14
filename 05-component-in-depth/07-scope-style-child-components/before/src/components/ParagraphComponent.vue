0a7879e9a8fac6db121604a6615f3734b0c0c202
Takashi2377 <aka05068@gmail.com>
2023-06-14 22:24:48 +0800

    finish

--- 05-component-in-depth/07-scope-style-child-components/before/src/components/ParagraphComponent.vue
+++ 05-component-in-depth/07-scope-style-child-components/before/src/components/ParagraphComponent.vue
@@ -1,5 +1,8 @@
 <template>
   <p><TextComponent>一些文字</TextComponent></p>
+  <p>
+    <TextComponent><div>slot 中的其他文字</div></TextComponent>
+  </p>
 </template>
 <script>
 import TextComponent from "./TextComponent.vue";
@@ -9,4 +12,24 @@ export default {
   },
 };
 </script>
-<style scoped></style>
+<style scoped>
+p {
+  background-color: rgb(74, 74, 124);
+  padding: 1em;
+  border-radius: 4px;
+  margin-bottom: 20px;
+}
+
+.text {
+  color: rgb(74, 156, 133);
+}
+
+.text :deep(a) {
+  display: block;
+  text-decoration: none;
+  color: rgb(80, 180, 137);
+}
+/* .text div {
+  color: rgb(241, 249, 28);
+} */
+</style>
--- 05-component-in-depth/07-scope-style-child-components/before/src/components/TextComponent.vue
+++ 05-component-in-depth/07-scope-style-child-components/before/src/components/TextComponent.vue
@@ -1,9 +1,14 @@
 <template>
   <span class="text">
     <slot />
+    <a href="#">非根元素的其他元素</a>
   </span>
 </template>
 <script>
 export default {};
 </script>
-<style scoped></style>
+<style scoped>
+:slotted(div) {
+  color: rgb(137, 53, 53);
+}
+</style>
