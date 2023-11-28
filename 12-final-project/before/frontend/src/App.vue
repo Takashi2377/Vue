2427d3cc7163c1105892b3c5d26e0c24c70de6e1
Takashi2377 <aka05068@gmail.com>
2023-11-28 23:23:55 +0800

    finish

---
+++ 12-final-project/before/frontend/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <TheIcon icon="home" />
+</template>
+
+<script setup>
+import "./assets/base.css";
+import TheIcon from "./components/TheIcon.vue";
+</script>
+
+<style scoped></style>
