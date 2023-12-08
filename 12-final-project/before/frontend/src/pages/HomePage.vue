ef7f35280ad694189faa6a546962980f240b6066
Takashi2377 <120803903+Takashi2377@users.noreply.github.com>
2023-12-08 23:51:08 +0800

    finish

--- 12-final-project/before/frontend/src/pages/HomePage.vue
+++ 12-final-project/before/frontend/src/pages/HomePage.vue
@@ -1,94 +1,16 @@
 <template>
   <div>
-    <div class="postList">
-      <div class="postItem" v-for="n in 10">
-        <img
-          src=""
-          alt=""
-          width="100%"
-          height="100%"
-          style="background: #eee"
-        />
-        <div class="postInfo">
-          <div class="postMeta">
-            <TheAvatar />
-            <span>張國棟</span>
-            <span class="postPubDate">12hrs before</span>
-            <PostActions />
-          </div>
-          <div class="postDesc">
-            <p>這到底甚麼閃現 吼吼吼吼吼</p>
-          </div>
-        </div>
-      </div>
-    </div>
+    <PostList>
+      <PostItem v-for="n in 10" />
+    </PostList>
     <!-- <PostDetails /> -->
     <!-- <PostUpload /> -->
   </div>
 </template>
 
 <script setup>
-import TheAvatar from "../components/TheAvatar.vue";
-import TheIcon from "../components/TheIcon.vue";
-import PostDetails from "../components/PostDetails.vue";
-import PostActions from "../components/PostActions.vue";
-import PostUpload from "../components/PostUpload.vue";
+import PostItem from "../components/PostItem.vue";
+import PostList from "../components/PostList.vue";
 </script>
 
-<style scoped>
-.postActions {
-  grid-area: actions;
-  justify-self: end;
-}
-
-.postItem {
-  box-shadow: 0px 12px 24px rgba(0, 0, 0, 0.08);
-  border-radius: 8px;
-}
-
-.postInfo {
-  padding: 24px;
-}
-
-.postItem > img {
-  width: 100%;
-  height: 400px;
-  object-fit: cover;
-  background: #eee;
-  cursor: pointer;
-}
-
-.postMeta {
-  display: grid;
-  grid-template-areas:
-    "avatar name actions"
-    "pubDate pubDate actions";
-  grid-template-columns: 42px 1fr 3fr;
-  row-gap: 6px;
-}
-.postMeta .avatar {
-  grid-area: avatar;
-}
-
-.postMeta .postPubDate {
-  grid-area: pubDate;
-  color: #9f9f9f;
-  font-size: 14px;
-}
-
-.postActions {
-  grid-area: actions;
-  justify-self: end;
-}
-
-.postDesc {
-  margin-top: 28px;
-  white-space: pre-line;
-}
-
-.postList {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 48px;
-}
-</style>
+<style scoped></style>
